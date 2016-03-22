07ab84c71f0db83c0c96be8c9a9f5bb0c3fc5f3f
RomeroFederico <romerof14@gmail.com>
2016-03-22 11:39:29 -0300

    Main y clase Validar creada.

---
+++ Ejercicio_11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_11
+{
+
+    /*
+    11.  Ingresar 10 números enteros que pueden estar dentro de un rango
+    de entre 100 y -100.
+    Para ello realizar una clase llamada 'Validacion' que posea un
+    método estático llamado Validar, que posea la siguiente firma:
+    bool Validar(int, int, int).
+    Terminado el ingreso mostrar el valor mínimo, el valor máximo y
+    el promedio.
+    Nota: Utilizar variables escalares, NO utilizar vectores.
+    */
+
+    class Ejercicio_11
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro. 11";
+        }
+    }
+}
