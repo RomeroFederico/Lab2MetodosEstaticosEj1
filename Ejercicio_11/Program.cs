cc7752a28b50b560c98bd093de19b94ac3fda084
RomeroFederico <romerof14@gmail.com>
2016-03-22 11:57:38 -0300

    Ejercicio completado

--- Ejercicio_11/Program.cs
+++ Ejercicio_11/Program.cs
@@ -22,6 +22,47 @@ namespace Ejercicio_11
         static void Main(string[] args)
         {
             Console.Title = "Ejercicio Nro. 11";
+
+            int numero;
+            int valorMinimo = 0;
+            int valorMaximo = 0;
+            double promedio = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                do
+                {
+                    Console.Write("Ingrese un numero entre el -100 y el 100: ");
+
+                    while (!int.TryParse(Console.ReadLine(), out numero))
+                    {
+                        Console.Clear();
+                        Console.Write("ERROR!!! No se ha ingresado un numero. Ingrese un numero entre el -100 y el 100: ");
+                    }
+
+                    Console.Clear();
+                    if (!Validacion.Validar(numero, -100, 100))
+                        Console.Write("ERROR!!! Numero no valido. ");
+                } while (!Validacion.Validar(numero, -100, 100));
+
+                if (i == 0)
+                {
+                    valorMaximo = numero;
+                    valorMinimo = numero;
+                }
+                else
+                {
+                    if (numero < valorMinimo)
+                        valorMinimo = numero;
+                    if (numero > valorMaximo)
+                        valorMaximo = numero;
+                }
+
+                promedio += numero;
+            }
+
+            Console.Clear();
+            Console.WriteLine("El valor minimo fue: {0}.\nEl valor maximo fue: {1}.\nEl promedio fue de {2,0:#,###.00}. ", valorMinimo, valorMaximo, promedio / 10);
         }
     }
 }
--- Ejercicio_11/Validacion.cs
+++ Ejercicio_11/Validacion.cs
@@ -7,5 +7,11 @@ namespace Ejercicio_11
 {
     class Validacion
     {
+        public static bool Validar(int numero, int rangoInferior, int rangoSuperior)
+        {
+            if (numero >= rangoInferior && numero <= rangoSuperior)
+                return true;
+            return false;
+        }
     }
 }
